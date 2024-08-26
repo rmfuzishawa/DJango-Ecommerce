75311697c49d828980f28122d804576f29803685
Richard Fuzishawa <rmfuzishawa@gmail.com>
2024-08-25 22:27:28 -0300

    Versão Final

--- loja/urls.py
+++ loja/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# TODO: Remover debug toolbar
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)), 
+    ] + urlpatterns
